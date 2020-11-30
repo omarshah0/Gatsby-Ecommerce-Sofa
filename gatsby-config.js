9db9ec865f8bc6b466978a86f7e21bb4d3f1d8f6
omarshah0 <oemyoem55@gmail.com>
2020-11-30 19:32:01 +0500

    Auth Updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
+    pathPrefix: "/Gatsby-Ecommerce-Sofa",
     title: `Gatsby JAMstack ECommerce Professional`,
     description: `Get up and running with your next E Commerce Website.`,
-    author: `@dabit3`,
+    author: `@omarfarooqshah`,
   },
   plugins: [
     {
